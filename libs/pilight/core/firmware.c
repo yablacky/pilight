3164bfdd738d41b54720b9d6f7d8b9cc2bb59fb7
Lutz Schwarz <schwarz.ware@gmx.de>
2016-04-13 10:06:31 +0200

    Use better names for V4 filter modes.

--- libs/pilight/core/firmware.c
+++ libs/pilight/core/firmware.c
@@ -1274,11 +1274,11 @@ firmware_t *firmware_from_hw(firmware_t *result, double version, double lpf, dou
 		method = 3;
 	}
 	static const char * const filter[] = {
-		"version 4, initial mode",	// never changed by firmware-control
+		"filter-V4, initial-mode",	// never changed by firmware-control
 		"no-operation",
 		"test-signal",
-		"version 3",
-		"version 4",
+		"filter-V3",
+		"filter-V4",
 	};
 	if (method < countof(filter)) {
 		fw.method = filter[method];
